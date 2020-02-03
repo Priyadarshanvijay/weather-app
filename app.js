b339462fc944209dc48406545bd0161d082d6752
Priyadarshan  Vijay <priyadarshan.v@talview.com>
2020-02-03 16:08:05 +0530

    Created forecasting functionality by chainig callbacks

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const request = require('request');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 geocode('Boston', (error,data) => {
     if(error){
-        console.log(error);
+        return console.log(error); //stops the function
     }
-    else{
-        console.log(`Location : ${data.location}\nLatitude: ${data.latitude}\nLongitude : ${data.longitude}`);
-    }
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if(error){
+            return console.log('Error', error);
+        }
+        console.log(data.location);
+        console.log(`${forecastData.summary} It is currently ${forecastData.temperature} degress out. There is a ${forecastData.chanceOfRain}% chance of rain.`);
+    });
 });
-
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
